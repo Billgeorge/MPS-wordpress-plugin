3109e1b4b5360a16e3c76144645b9c2989ce5838
Leonardo Espinosa <jorjek4@hotmail.com>
2021-01-28 10:55:23 -0500

    MPS-creating base repository for php

---
+++ woo-mipagoseguro-payment/index.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+    Plugin Name: woo-mipagoseguro-payment
+    Plugin URI: https://mipagoseguro.co
+    Description: Este plugin permite mostrar mipagoseguro en las tiendas woocommerce.
+    Version:1.0
+    Author:Leonardo Espinosa
+    Author URI:https://mipagoseguro.co
+    License:GPLv2 o versión más reciente
+*/
+?>
